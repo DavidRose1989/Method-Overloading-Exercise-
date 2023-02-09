36d75026ae191bb6492476e89908ab2584b5140e
DavidRose <DavidRose331@Gmail.com>
2023-02-08 23:28:46 -0600

    initial

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace MethodOverloading
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Add(int num1, int num2)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(num1 + num2);
         }
+
+        public static void Add(decimal num1, decimal num2, decimal num3)
+        { 
+            Console.WriteLine(num1 + num2 + num3);
+        }
+
+        public static string Add(int num5, int num6, bool numtrue)
+        {
+            
+
+            if(numtrue is true && (num5 + num6) != 1)
+            {
+               return ($" {num5 + num6} dollars");
+            }
+           else if (numtrue is true && (num5 + num6)  == 1)
+            {
+               return($"{num5 + num6} dollar");
+            }
+            else 
+            { 
+            return $"{num5+ num6}";
+            }
+        
+
+        }
+        
+        static void Main()
+        {
+            Console.WriteLine(Add(0, 1, true));
+        }
+
+            
     }
 }
